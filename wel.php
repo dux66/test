7a688c5e536ad8bf670e969a4b6857ee188196e5
dux66 <43168193+dux66@users.noreply.github.com>
2020-07-16 18:46:25 +0800

    Update wel.php

--- wel.php
+++ wel.php
@@ -4,7 +4,7 @@ session_start();
 echo '登录成功<br />';
 
 echo 'hello，'.$_SESSION['name'].',welcome to login<br />';
-echo "这是一个jenkins自动发包的测试  20200716  18：21<br />” ；
-echo "这是一个jenkins自动发包的测试  20200716  18:44<br />” ；
+echo "这是一个jenkins自动发包的测试  20200716  18：21<br />" ；
+echo "这是一个jenkins自动发包的测试  20200716  18:44<br />" ；
 
 ?>
